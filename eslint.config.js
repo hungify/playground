9576ec85e930729848e46e36a639034dc60d2491
hungify <nmhungify@gmail.com>
2023-12-09 10:55:29 +0700

    feat: enable several flag for strictness in tsconfig

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,17 @@
-import { all } from '@sxzz/eslint-config'
+import { GLOB_TS, GLOB_TSX, sxzz } from '@sxzz/eslint-config'
 
-export default all
+export default sxzz(
+  {
+    files: [GLOB_TS, GLOB_TSX],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+    },
+  },
+  {
+    vue: true,
+    prettier: true,
+    markdown: true,
+    unocss: true,
+    sortKeys: true,
+  },
+)
